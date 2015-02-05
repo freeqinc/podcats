51a6b30852c84dc8ae5fb85817c39adff34d841a
Jesse <itzjesse@gmail.com>
2015-02-05 15:21:19 -0800

    Asdf

---
+++ static/js/importnav.js
@@ -0,0 +1,22 @@
+function importnav(){
+	document.write("	<div class=\"wrapper home-wrapper\">");
+	document.write("		<div class=\"navigation\">");
+	document.write("			<div class=\"menu-button\">");
+	document.write("				<span class=\"icon-list burger-icon\"><\/span>");
+	document.write("			<\/div>");
+	document.write("			<div class=\"bread link-button\"><img class=\"nav-logo\" src=\".\/assets\/white_cat.png\"><a href=\".\/home.html\" class=\"hidden-link\"><\/a><\/div>");
+	document.write("		<\/div>");
+	document.write("");
+	document.write("	<\/div>");
+	document.write("	<div class=\"nav-menu\">");
+	document.write("		<span class=\"icon-close menu-close\"><\/span>");
+	document.write("		<img src=\".\/assets\/orange_cat.png\" class=\"orange-cat\" alt=\"orange-cat\">");
+	document.write("		<div class=\"menu-items\">");
+	document.write("			<div class=\"menu-item first-menu-item link-button\">Home<a href=\".\/home.html\" class=\"hidden-link\"><\/a><\/div>");
+	document.write("			<div class=\"menu-item\">Help<\/div>");
+	document.write("			<div class=\"menu-item\">Settings<\/div>");
+	document.write("			<div class=\"menu-item logout link-button\">Logout<a href=\".\/index.html\" class=\"hidden-link\"><\/a><\/div>");
+	document.write("		<\/div>");
+	document.write("	<\/div>");
+
+}
