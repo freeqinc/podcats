099ce725c5900cf6f346373327d9fa74a3f84ed7
Jesse <itzjesse@gmail.com>
2015-02-12 05:00:14 -0800

    asjgklewgjawl will keep working later i wanna sleep cry

--- static/js/importnav.js
+++ static/js/importnav.js
@@ -12,11 +12,21 @@ function importnav(){
 	document.write("		<span class=\"icon-close menu-close\"><\/span>");
 	document.write("		<img src=\".\/assets\/orange_cat.png\" class=\"orange-cat\" alt=\"orange-cat\">");
 	document.write("		<div class=\"menu-items\">");
-	document.write("			<div class=\"menu-item first-menu-item link-button\">Home<a href=\".\/home.html\" class=\"hidden-link\"><\/a><\/div>");
-	document.write("			<div class=\"menu-item\">Help<\/div>");
-	document.write("			<div class=\"menu-item\">Settings<\/div>");
-	document.write("			<div class=\"menu-item logout link-button\">Logout<a href=\".\/index.html\" class=\"hidden-link\"><\/a><\/div>");
+	document.write("			<div class=\"menu-item first-menu-item link-button\">");
+	document.write("				Home<a href=\".\/home.html\" class=\"hidden-link\"><\/a>");
+	document.write("			<\/div>");
+	document.write("			<div class=\"menu-item link-button\">");
+	document.write("				Help<a href=\".\/help.html\" class=\"hidden-link\"><\/a>");
+	document.write("			<\/div>");
+	document.write("			<div class=\"menu-item link-button\">");
+	document.write("				Settings<a href=\".\/settings.html\" class=\"hidden-link\"><\/a>");
+	document.write("			<\/div>");
+	document.write("			<div class=\"menu-item logout link-button\">");
+	document.write("				Logout<a href=\".\/index.html\" class=\"hidden-link\"><\/a>");
+	document.write("			<\/div>");
 	document.write("		<\/div>");
 	document.write("	<\/div>");
+	document.write("");
+
 
 }
---
+++ static/js/nav-animation.js
@@ -0,0 +1,48 @@
+$(window).load(function() {
+
+	var burger = true;
+	var burgTime = 150;
+	var burgType = "linear";
+	$('.hamburger-button').click(function(){
+		if(burger){
+			burger = false;
+			$('.burger-1').velocity({
+				rotateZ : '45deg',
+				top: 10
+			},{
+				duration: burgTime,
+				easing: burgType
+			});
+			$('.burger-2').velocity({
+				opacity: 0
+			},{
+				duration: burgTime,
+				easing: burgType
+			});
+			$('.burger-3').velocity({
+				rotateZ : '-45deg',
+				top: 10
+			},{
+				duration: burgTime,
+				easing: burgType
+			});
+			$('.navigation-expand').velocity({
+				height: 110
+			},{
+				duration: burgTime,
+				easing: burgType
+			});
+			$('.page-wrap').velocity({
+				top: 170
+			},{
+				duration: burgTime,
+				easing: burgType
+			});
+		}
+		else{
+			burger = true;
+			$('.burger-1, .burger-2, .burger-3, .navigation-expand, .page-wrap').velocity('reverse',burgTime);
+		}
+	});
+
+});
