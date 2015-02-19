9a6d75a89aa2148893f017ae861db8c0d2f9732e
Jesse <itzjesse@gmail.com>
2015-02-19 12:42:19 -0800

    changed renaming lectures

--- public/js/rename.js
+++ public/js/rename.js
@@ -1,29 +1,57 @@
 $(window).load(function() {
 
+
+
+	function placeCaretAtEnd(el) {
+		if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+			var range = document.createRange();
+			range.selectNodeContents(el);
+			range.collapse(false);
+			var sel = window.getSelection();
+			sel.removeAllRanges();
+			sel.addRange(range);
+		} 
+		else if (typeof document.body.createTextRange != "undefined") {
+			var textRange = document.body.createTextRange();
+			textRange.moveToElementText(el);
+			textRange.collapse(false);
+			textRange.select();
+		}
+	}
+
+
 	var renaming = false;
 
 	$('.ce1-text').click(function(){
 		var rename = $(this).find('.rename-text');
 		var lecture = $(this).closest('.course').find('.lecture-name');
-		rename.attr('contenteditable','true');
-		rename.html("");
-		rename.focus();
-		lecture.html("Click here when done!");
-		renaming = true;
-		return false;
-	});
-
-	$('.course').click(function(){
-		if(renaming){
-			var rename = $(this).find('.rename-text');
-			var lecture = $(this).find('.lecture-name');
-
-			lecture.html(rename.html());
-			rename.html('Rename');
-			rename.attr('contenteditable','false');
-
+		if(!renaming){
+			lecture.attr('contenteditable','true');
+			lecture.focus();
+			placeCaretAtEnd(lecture.get(0));
+			rename.html("Click here when done!");
+			renaming = true;
+			return false;
+		}
+		else{
 			renaming = false;
+			rename.html("Rename");
 		}
 	});
+
+
+
+	// $('.course').click(function(){
+	// 	if(renaming){
+	// 		var rename = $(this).find('.rename-text');
+	// 		var lecture = $(this).find('.lecture-name');
+
+	// 		lecture.html(rename.html());
+	// 		rename.html('Rename');
+	// 		rename.attr('contenteditable','false');
+
+	// 		renaming = false;
+	// 	}
+	// });
 
 });
