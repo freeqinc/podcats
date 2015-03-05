c11e8d8bf1ea8e29110f24d41d63764df7e5df18
Jesse <itzjesse@gmail.com>
2015-03-05 01:38:33 -0800

    asdf

--- public/js/importnav.js
+++ public/js/importnav.js
@@ -11,9 +11,10 @@ function importnav(){
 	document.write("			<div class=\"burger-2 burger-line\"><\/div>");
 	document.write("			<div class=\"burger-3 burger-line\"><\/div>");
 	document.write("		<\/div>");
-	document.write("	<div class=\"nav-logo link-button\">");
-	document.write("			POD<span class=\"logo-bold\">CATS<\/span> ");
-	document.write("			<a href=\"/classes\" class=\"hidden-link\"><\/a>");
+	document.write("		<div class=\"nav-logo link-button\">");
+	document.write("			<img class=\"nav-img\" src=\".\/images\/white_cat.png\">");
+	document.write("	<!-- 		POD<span class=\"logo-bold\">CATS<\/span>  -->");
+	document.write("			<a href=\"\/classes\" class=\"hidden-link\"><\/a>");
 	document.write("		<\/div>");
 	document.write("	<\/div>");
 	document.write("	<div class=\"navigation-expand\">");
